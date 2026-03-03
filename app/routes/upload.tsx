659d35884931f4eefb6a696f9c49e5968cea0792
Ian McFarland <ian.mcfarland8@gmail.com>
2026-03-03 16:44:44 +0100

    feat: Resume Analysis features: file upload, conversion to img, and AI Analysis using Puter.js

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -2,8 +2,15 @@ import { useState } from 'react'
 import Navbar from '~/components/navbar'
 import FileUploader from '~/components/FileUploader'
 import type { FormEvent } from 'react'
+import { usePuterStore } from '~/lib/puter'
+import { useNavigate } from 'react-router'
+import { convertPdfToImage } from '~/lib/pdf2img'
+import { generateUuid } from '~/utils'
+import { prepareInstructions } from 'constants/index'
 
 const upload = () => {
+    const { auth, isLoading, fs, ai, kv } = usePuterStore()
+    const navigate = useNavigate()
     const [isProcessing, setIsProcessing] = useState(false)
     const [statusText, setStatusText] = useState("")
     const [file, setFile] = useState<File | null>(null)
@@ -12,17 +19,98 @@ const upload = () => {
         setFile(file)
     }
 
+    const handleAnalyze = async ({companyName, jobTitle, jobDescription, file}: {companyName: string, jobTitle: string, jobDescription: string, file: File | null}) => {
+        if (!file) {
+            setStatusText("No file selected")
+            return
+        }
+
+        setIsProcessing(true)
+        setStatusText("Uploading your resume pdf...")
+
+        const uploadedFile = await fs.upload([file])
+
+        if (!uploadedFile) {
+            setIsProcessing(false)
+            setStatusText("Failed to upload your resume")
+            return
+        }
+
+        setStatusText("Converting your resume to an image...")
+        let imgFile: File | null = null
+        if (file) {
+            const conversionResult = await convertPdfToImage(file)
+            imgFile = conversionResult.file
+            console.log("Image converted:", imgFile)
+            if (conversionResult.error) {
+                console.error("PDF conversion error:", conversionResult.error)
+            }
+        }
+
+        if (!imgFile) {
+            setIsProcessing(false)
+            setStatusText("Failed to convert your resume to an image")
+            return
+        }
+
+        setStatusText("Uploading your resume image...")
+        const uploadedImage = await fs.upload([imgFile])
+
+        if (!uploadedImage) {
+            setStatusText("Failed to upload your resume image")
+            return setIsProcessing(false)
+        }
+
+        setStatusText("Preparing your resume for analysis...")
+        const uuid = generateUuid()
+        const data = {
+            uuid,
+            resumePath: uploadedFile.path,
+            imagePath: uploadedImage.path,
+            companyName,
+            jobTitle,
+            jobDescription,
+            feedback: "",
+        }
+
+        await kv.set(`resume:${uuid}`, JSON.stringify(data))
+
+        setStatusText("Analyzing your resume...")
+
+        const feedback = await ai.feedback(
+            uploadedFile.path,
+            prepareInstructions({jobTitle, jobDescription})
+        )
+
+        if (!feedback) {
+            setIsProcessing(false)
+            setStatusText("Failed to analyze your resume")
+            return
+        }
+            
+        const feedbackText = typeof feedback === 'string' 
+        ? feedback.message.content
+        : feedback.message.content[0].text;
+
+        data.feedback = JSON.parse(feedbackText)
+        await kv.set(`resume:${uuid}`, JSON.stringify(data))
+        setStatusText("Analysis complete! Redirecting...")
+        console.log(data)        
+    }
+
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault()
         const form = e.currentTarget.closest('form')
         if (!form) return
         const formData = new FormData(form)
 
-        const companyName = formData.get('company-name')
-        const jobTitle = formData.get('job-title')
-        const jobDescription = formData.get('job-description')
+        const companyName = formData.get('company-name') as string
+        const jobTitle = formData.get('job-title') as string
+        const jobDescription = formData.get('job-description') as string
 
-        console.log({companyName, jobTitle, jobDescription, file})
+        if (!file) return;
+
+        handleAnalyze({companyName, jobTitle, jobDescription, file})
     }
         
 
--- app/utils.ts
+++ app/utils.ts
@@ -5,3 +5,7 @@ export const formatSize = (bytes: number) => {
     const i = Math.floor(Math.log(bytes) / Math.log(k))
     return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i]
 }
+
+export const generateUuid = () => {
+    return crypto.randomUUID()
+}
